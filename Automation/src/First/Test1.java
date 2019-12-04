ea144bdc0f89bf919589674dd0241383bb0213da
SandyReturns <52030246+SandyReturns@users.noreply.github.com>
2019-12-04 12:41:40 +0530

    Update Test1.java

--- Automation/src/First/Test1.java
+++ Automation/src/First/Test1.java
@@ -4,7 +4,9 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int num=10,num1=20;
+		int sum=num+num2;
+		System.out.println(sum);
 	}
 
 }
